71cc785e7286da4f68cde765ce257bbb3c26febd
RyumForCode <ryumforart@gmail.com>
2025-01-23 23:07:35 +0900

    feat: style.ts implemented for Header component

---
+++ src/components/Header/style.ts
@@ -0,0 +1,63 @@
+const header_style_object = {
+  wrapper_style:
+    "w-full h-fit p-[8px] " + "flex flex-row justify-between items-center ",
+  logo_container_style:
+    "w-[238px] pr-[25px] " +
+    "flex flex-row justify-start items-center shrink-0 ",
+  interaction_container_style:
+    "w-full " + "flex flex-row justify-between items-center shrink-1 ",
+  user_info_container_style:
+    "px-[2px_4px] " + "flex flex-row justify-start items-center shrink-0 ",
+  menu_icon_button:
+    "p-[12px] m-[0_4px] " +
+    "rounded-full " +
+    "cursor-pointer hover:bg-[var(--color-hoverGray)] ",
+  google_calendar_logo_icon_style:
+    "w-[40px] h-[40px] pr-[4px] my-[4px] box-content ",
+  google_calendar_logo_type_style:
+    "pl-[4px] " +
+    "text-[22px] font-[Google_Sans] color-[var(--color-logoGray)] select-none ",
+  date_controller_style:
+    "ml-[8px] " + "flex flex-row justify-between items-center ",
+  function_controller_style:
+    "px-[6px] " + "flex flex-row justify-center items-center ",
+  function_icon_style:
+    "p-[8px] rounded-full " +
+    "cursor-pointer hover:bg-[var(--color-hoverGray)] duration-[100ms] ",
+  other_service_menu_icon_style:
+    "m-[4px_2px_4px_0] p-[8px] rounded-full " +
+    "cursor-pointer hover:bg-[var(--color-hoverGray)] duration-[100ms] ",
+  profile_button_style:
+    "w-[32px] h-[32px] m-[8px_6px_8px_10px] rounded-full bg-[#43accc] " +
+    "font-[Google_Sans] text-[var(--color-white)] " +
+    "cursor-pointer outline-[4px] hover:outline-[var(--color-hoverGray)] duration-[100ms] ",
+  today_finder_button_style:
+    "h-[40px] px-[24px] m-[4px_20px_4px_0] " +
+    "border-[1px] border-[var(--color-border)] rounded-full " +
+    "text-[14px] " +
+    "cursor-pointer hover:bg-[var(--color-hoverGray)] duration-[100ms] ",
+  unit_change_arrow_icon_style:
+    "p-[4px] rounded-full " +
+    "cursor-pointer hover:bg-[var(--color-hoverGray)] duration-[100ms] ",
+  current_date_indicator_style:
+    "p-[8px] mx-[11px] " + "font-[Google_Sans] text-[21.5px] select-none ",
+  calendar_unit_dropdown_button_style:
+    "h-[40px] ml-[12px] my-[4px] px-[17px] " +
+    "flex flex-row justify-center items-center " +
+    "border-[1px] border-[var(--color-border)] rounded-full " +
+    "text-[14px] " +
+    "cursor-pointer hover:bg-[var(--color-hoverGray)] duration-[100ms] ",
+  view_selection_fieldset_style:
+    "ml-[12px] " + "flex flex-row justify-center items-center ",
+  view_selection_input_left_style:
+    "p-[9px_14px_9px_18px] bg-[#c2e7ff] " +
+    "border-y-[1px] border-l-[1px] border-[var(--color-border)] rounded-[20px_0_0_20px] " +
+    "cursor-pointer hover:bg-[var(--color-hoverGray)] duration-[100ms] ",
+  view_selection_input_right_style:
+    "p-[9px_18px_9px_14px] " +
+    "border-y-[1px] border-r-[1px] border-[var(--color-border)] rounded-[0_20px_20px_0] " +
+    "cursor-pointer hover:bg-[var(--color-hoverGray)] duration-[100ms] ",
+  view_selection_divider_style: "w-[1px] h-[40px] bg-[var(--color-border)] ",
+};
+
+export default header_style_object;
