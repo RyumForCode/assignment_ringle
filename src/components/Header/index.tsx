e4ebad1f66ce5adad40e5a8d8cc732faee1a32f6
RyumForCode <ryumforart@gmail.com>
2025-01-23 23:08:00 +0900

    feat: Header component layout and style implemented

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,85 @@
+import header_style_object from "./style";
+
 export const Header = () => {
-  return <header></header>;
+  return (
+    <header className={header_style_object.wrapper_style}>
+      <div className={header_style_object.logo_container_style}>
+        <button className={header_style_object.menu_icon_button}>
+          <img src="/icons/menu.svg" alt="menu icon" width={24} height={24} />
+        </button>
+        <img
+          className={header_style_object.google_calendar_logo_icon_style}
+          src="/icons/google_calendar.png"
+          alt="google calendar icon"
+        />
+        <span className={header_style_object.google_calendar_logo_type_style}>
+          Calendar
+        </span>
+      </div>
+      <div className={header_style_object.interaction_container_style}>
+        <div className={header_style_object.date_controller_style}>
+          <button className={header_style_object.today_finder_button_style}>
+            오늘
+          </button>
+          <button className={header_style_object.unit_change_arrow_icon_style}>
+            <img src="/icons/chevron_left.svg" alt="chevron left icon" />
+          </button>
+          <button className={header_style_object.unit_change_arrow_icon_style}>
+            <img src="/icons/chevron_right.svg" alt="chevron right icon" />
+          </button>
+          <span className={header_style_object.current_date_indicator_style}>
+            2025년 1월
+          </span>
+        </div>
+        <div className={header_style_object.function_controller_style}>
+          <button className={header_style_object.function_icon_style}>
+            <img src="/icons/search.svg" alt="search icon" />
+          </button>
+          <button
+            className={header_style_object.function_icon_style + "mx-[2px] "}
+          >
+            <img src="/icons/help.svg" alt="help icon" />
+          </button>
+          <button
+            className={header_style_object.function_icon_style + "ml-[4px] "}
+          >
+            <img src="/icons/settings.svg" alt="settings icon" />
+          </button>
+          <button
+            className={header_style_object.calendar_unit_dropdown_button_style}
+          >
+            주
+            <img
+              className="pl-[8px]"
+              src="/icons/arrow_drop_down.svg"
+              alt="arrow drop down icon"
+            />
+          </button>
+          <fieldset
+            className={header_style_object.view_selection_fieldset_style}
+          >
+            <label
+              className={header_style_object.view_selection_input_left_style}
+            >
+              <input type="radio" name="job_selection" hidden />
+              <img src="/icons/date_range.svg" alt="date range icon" />
+            </label>
+            <div className={header_style_object.view_selection_divider_style} />
+            <label
+              className={header_style_object.view_selection_input_right_style}
+            >
+              <input type="radio" name="job_selection" hidden />
+              <img src="/icons/task_alt.svg" alt="task alt icon" />
+            </label>
+          </fieldset>
+        </div>
+      </div>
+      <div className={header_style_object.user_info_container_style}>
+        <button className={header_style_object.other_service_menu_icon_style}>
+          <img src="/icons/apps.svg" alt="apps icon" />
+        </button>
+        <button className={header_style_object.profile_button_style}>AB</button>
+      </div>
+    </header>
+  );
 };
