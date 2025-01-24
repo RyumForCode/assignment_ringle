ba5edc03b9fabb792b3f83ded60a49b7bb0be30b
RyumForCode <ryumforart@gmail.com>
2025-01-24 15:49:49 +0900

    feat: utils object created and calendarGenerator function implemented

---
+++ src/utils/index.ts
@@ -0,0 +1,21 @@
+const utils = {
+  calendarGenerator: (year: number, month: number) => {
+    const firstDayOfMonth = new Date(year, month, 1);
+    const startDay = firstDayOfMonth.getDay();
+    const calendar = [];
+
+    const current = new Date(year, month, 1 - startDay);
+
+    for (let week = 0; week < 6; week++) {
+      const weekBatch = [];
+      for (let day = 0; day < 7; day++) {
+        weekBatch.push(new Date(current));
+        current.setDate(current.getDate() + 1);
+      }
+      calendar.push(weekBatch);
+    }
+
+    return calendar;
+  },
+
+export default utils;
