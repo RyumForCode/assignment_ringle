8bb1faad067774e7a8f0abd272610492920d351a
RyumForCode <ryumforart@gmail.com>
2025-01-26 04:02:52 +0900

    feat: ScheduleCard component onClickScheduleCard eventhandler added & refactored timeRangeDisplay

--- src/components/ScheduleCard/index.tsx
+++ src/components/ScheduleCard/index.tsx
@@ -1,4 +1,6 @@
-import { useMemo } from "react";
+import React, { useMemo, useRef } from "react";
+import { useDispatch } from "react-redux";
+import { openModal } from "../../store/inspectSlice";
 import { ScheduleObject } from "../../store/scheduleSlice";
 import utils from "../../utils";
 import style_object from "./style";
@@ -10,6 +12,10 @@ export const ScheduleCard = ({
   scheduleObject: ScheduleObject;
   date: Date;
 }) => {
+  const ref = useRef<HTMLButtonElement>(null);
+
+  const dispatch = useDispatch();
+
   // Memoization card style
   const cardStyleObject = useMemo(() => {
     const { topRatio, bottomRatio } = utils.calculateCardPosition(
@@ -28,23 +34,29 @@ export const ScheduleCard = ({
     const { startAtIsoString, endToIsoString } = scheduleObject;
     const startAt = utils.parseISOToDate(startAtIsoString);
     const endTo = utils.parseISOToDate(endToIsoString);
-    const startTimeDisplay = utils.inputTimeParser.timeInput(startAt);
-    const endTimeDisplay = utils.inputTimeParser.timeInput(endTo);
 
-    if (startTimeDisplay.split(" ")[0] === endTimeDisplay.split(" ")[0]) {
-      return `${startTimeDisplay}~${endTimeDisplay.split(" ")[1]}`;
-    }
-    return `${startTimeDisplay}~${endTimeDisplay}`;
+    return utils.timeRangeDisplayParser(startAt, endTo);
   }, [scheduleObject]);
+
+  // onClick event for schedule card to open inspect modal
+  const onClickScheduleCard = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    if (!ref.current) return;
+    const { top, right } = ref.current.getBoundingClientRect();
+    const payload = {
+      schedule: scheduleObject,
+      position: { x: right + 8, y: top },
+    };
+    dispatch(openModal(payload));
+  };
 
   return (
     <button
+      ref={ref}
       className={style_object.card_button_style}
       style={cardStyleObject}
       key={scheduleObject.uid}
-      onClick={(e) => {
-        e.stopPropagation();
-      }}
+      onClick={onClickScheduleCard}
     >
       <span>{scheduleObject.title}</span>
       <span>{timeRangeDisplay}</span>
