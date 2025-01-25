da8d27b872abe8cd07260a4f4e3f2a1c21b461d8
RyumForCode <ryumforart@gmail.com>
2025-01-25 21:48:45 +0900

    feat: schedule_card_style_object created & implemented

---
+++ src/components/ScheduleCard/style.ts
@@ -0,0 +1,9 @@
+const schedule_card_style_object = {
+  card_button_style:
+    "absolute w-[calc(100%-12px)] p-[4px_8px_4px_8px] bg-[#039BE5] rounded-[6px] " +
+    "flex flex-col justify-start items-start " +
+    "font-[Google_Sans] text-[12px] text-[var(--color-white)] text-start " +
+    "cursor-pointer ",
+};
+
+export default schedule_card_style_object;
