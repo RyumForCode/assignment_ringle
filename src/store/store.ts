df600cf7f146f76a5af6398dd726f373a37cb6b6
RyumForCode <ryumforart@gmail.com>
2025-01-26 03:58:21 +0900

    feat: inspectReducer added on redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import calendarReducer from "./calendarSlice";
 import currentDateReducer from "./currentDateSlice";
+import inspectReducer from "./inspectSlice";
 import modalReducer from "./modalSlice";
 import scheduleReducer from "./scheduleSlice";
 
 export const store = configureStore({
   reducer: {
-    currentDate: currentDateReducer,
     calendar: calendarReducer,
+    currentDate: currentDateReducer,
+    inspect: inspectReducer,
     modal: modalReducer,
     schedule: scheduleReducer,
   },
