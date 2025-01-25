9a0ff10da245a92150d2db8e8ba7f5f2982d7ea6
RyumForCode <ryumforart@gmail.com>
2025-01-26 01:01:51 +0900

    feat: Header component next/prev week button event implemented

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,25 @@
+import { useSelector } from "react-redux";
 import { useSelectDate } from "../../hooks/useSelectDate";
+import { RootState } from "../../store/store";
+import utils from "../../utils";
 import header_style_object from "./style";
 
 export const Header = () => {
+  const { currentDate } = useSelector((state: RootState) => state.currentDate);
+
   // Custom hook for change calendar via date selection from calendar
   const selectDate = useSelectDate();
 
   // Event handler for date button elements
   const onClickTodayButton = () => {
     selectDate(new Date());
+  };
+
+  // onClick event for arrow button to move next/prev week
+  const onClickMoveWeek = (delta: -1 | 1) => {
+    const date = utils.parseISOToDate(currentDate);
+    date.setDate(date.getDate() + 7 * delta);
+    selectDate(date);
   };
 
   return (
@@ -33,10 +45,16 @@ export const Header = () => {
           >
             오늘
           </button>
-          <button className={header_style_object.unit_change_arrow_icon_style}>
+          <button
+            className={header_style_object.unit_change_arrow_icon_style}
+            onClick={() => onClickMoveWeek(-1)}
+          >
             <img src="/icons/chevron_left.svg" alt="chevron left icon" />
           </button>
-          <button className={header_style_object.unit_change_arrow_icon_style}>
+          <button
+            className={header_style_object.unit_change_arrow_icon_style}
+            onClick={() => onClickMoveWeek(1)}
+          >
             <img src="/icons/chevron_right.svg" alt="chevron right icon" />
           </button>
           <span className={header_style_object.current_date_indicator_style}>
