7af7fc676a6dd48af4bde0007acbb3aa51bad6ad
RyumForCode <ryumforart@gmail.com>
2025-01-25 22:42:55 +0900

    feat: import SchedulePanel component on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { DatePicker } from "./components/DatePicker";
 import { Header } from "./components/Header";
 import { MainLayout } from "./components/MainLayout";
+import { SchedulePanel } from "./components/SchedulePanel";
 import { SidePanel } from "./components/SidePanel";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <MainLayout>
         <DatePicker />
+        <SchedulePanel />
         <SidePanel />
       </MainLayout>
-    </div>
+    </>
   );
 }
 
